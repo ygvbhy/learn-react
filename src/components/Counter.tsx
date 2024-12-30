50bc6fbb7ff03f3403775b1fcb4a035b91b7ff22
ygvbhy <ygvbhy@naver.com>
2024-12-30 16:59:23 +0900

    state 실습

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,12 @@
-const Counter = () => {
-  return <button>Counter</button>;
+import { useState } from "react";
+
+const Counter = ({ handleTotal }: { handleTotal: () => void }) => {
+  const [counter, setCounter] = useState(0);
+  const handleCounter = () => {
+    setCounter(counter + 1);
+    handleTotal();
+  };
+  return <button onClick={handleCounter}>Counter: {counter}</button>;
 };
 
 export default Counter;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import Counter from "./Counter";
 
 const Main = () => {
+  const [total, setTotal] = useState(0);
+
+  const handleTotal = () => {
+    setTotal(total + 1);
+  };
   return (
     <main>
-      <Counter />
+      <Counter handleTotal={handleTotal} />
+      <br />
+      <br />
+      <Counter handleTotal={handleTotal} />
+      <br />
+      <br />
+      <p>Total: {total}</p>
     </main>
   );
 };
