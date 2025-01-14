21f65b1c19611c1fa9a0cbdb1506fd6e6c4e57f7
ygvbhy <ygvbhy@naver.com>
2025-01-14 22:24:47 +0900

    context 리팩토링

--- src/AppTheme.tsx
+++ src/AppTheme.tsx
@@ -2,23 +2,15 @@ import "./AppTheme.css";
 import HeaderTheme from "./components/theme/Header";
 import MainTheme from "./components/theme/Main";
 import FooterTheme from "./components/theme/Footer";
-import { DarkModeContext } from "./context/DarkModeContext";
-import { useState } from "react";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 function AppTheme() {
-  // 현재 테마 상태 값
-  const [theme, setTheme] = useState("light");
-  // 테마 변경 함수
-  const toggleDarkMode = () => {
-    // 이전 상태 값을 받아서 테마 변경
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  };
   return (
-    <DarkModeContext.Provider value={{ theme, toggleDarkMode }}>
+    <DarkModeProvider initTheme={"light"}>
       <HeaderTheme />
       <MainTheme />
       <FooterTheme />
-    </DarkModeContext.Provider>
+    </DarkModeProvider>
   );
 }
 
--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 // 컨텍스트 생성
 // theme 의 값을 boolean 으로 받지 않고 string 으로 받아서 진행
@@ -6,3 +6,24 @@ export const DarkModeContext = createContext({
   theme: "light",
   toggleDarkMode: () => {},
 });
+
+export const DarkModeProvider = ({
+  children,
+  initTheme = "light",
+}: {
+  children: React.ReactNode;
+  initTheme: string;
+}) => {
+  // 현재 테마 상태 값
+  const [theme, setTheme] = useState(initTheme);
+  // 테마 변경 함수
+  const toggleDarkMode = () => {
+    // 이전 상태 값을 받아서 테마 변경
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+  return (
+    <DarkModeContext.Provider value={{ theme, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
